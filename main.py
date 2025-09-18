aa775c0ad3c2ae8e8309fb52523a08a9b4847d18
LYX000000 <1729627758@qq.com>
2025-09-18 18:54:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# # 地球体重初始值（单位：公斤）
+# 地球体重初始值（单位：公斤）
 earth_weight = float(input("请输入您当前的体重（kg）：")) #字符串转换为浮点数
 # 月球体重占地球体重的16.5%
 moon_ratio = 0.165
@@ -9,4 +9,4 @@ for year in range(1, 11):
     current_earth_weight = earth_weight + 0.5 * year
     current_moon_weight = current_earth_weight * moon_ratio
     # 输出格式控制
-    print("{:<4}\t{:>12.2f}\t{:>12.2f}".format(year, current_earth_weight, current_moon_weight))在这个文件里编写代码
+    print("{:<4}\t{:>12.2f}\t{:>12.2f}".format(year, current_earth_weight, current_moon_weight))
